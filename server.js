3ff462957ba05d7b2549d86cb4134eadbe72e75a
John Janetka <70124466+jjanetka@users.noreply.github.com>
2026-06-28 14:40:04 -0700

    Connect to Render PostgreSQL and print usernames from test table

--- server.js
+++ server.js
@@ -1,10 +1,30 @@
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const { Client } = require('pg');
 
 const PORT = process.env.PORT || 3000;
 const filePath = path.join(__dirname, 'public', 'index.html');
 const indexHtml = fs.readFileSync(filePath);
+
+const client = new Client({
+  connectionString: 'postgresql://pguser:2MBjSQ4ZLhWkv567vNlotgvDEGMRe8zq@dpg-d90ok6u7r5hc73bcksp0-a/pgdb_1f14',
+});
+
+client.connect()
+  .then(() => {
+    console.log('Connected to PostgreSQL database');
+    return client.query('SELECT username FROM test');
+  })
+  .then((result) => {
+    console.log('Usernames in test table:');
+    result.rows.forEach((row) => {
+      console.log(row.username);
+    });
+  })
+  .catch((err) => {
+    console.error('Database error:', err);
+  });
 
 const server = http.createServer((req, res) => {
   if (req.url !== '/') {
